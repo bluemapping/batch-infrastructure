956341206696334e62a69e67f65fe870b7751603
Romain Cambonie <romain.cambonie@gmail.com>
2023-10-19 17:50:41 +0200

    test: compute environment

--- batch.tf
+++ batch.tf
@@ -3,8 +3,7 @@ resource "aws_batch_compute_environment" "edge" {
 
   compute_resources {
     ec2_configuration {
-      image_type        = "EC2"
-      image_id_override = "ami-0c47a507d2c485dff"
+      image_type = "ECS_AL2_NVIDIA"
     }
 
     instance_role = aws_iam_instance_profile.ecs_compute_instance_role.arn
@@ -13,17 +12,22 @@ resource "aws_batch_compute_environment" "edge" {
       "g3s.xlarge",
     ]
 
-    max_vcpus = 1
+    min_vcpus     = 0
+    max_vcpus     = 1
+    desired_vcpus = 0
 
     subnets = [
       var.private_subnets_ids[0]
     ]
+
+    security_group_ids = ["sg-0d7939fc6ec728231"]
 
     type = "EC2"
   }
 
   service_role = aws_iam_role.aws_batch_service_role.arn
   type         = "MANAGED"
+  state        = "ENABLED"
   depends_on   = [aws_iam_role_policy_attachment.aws_batch_service_role]
 
   tags = local.tags
